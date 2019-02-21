f1d3f0b76ab4239b1625458d35d7395c6c235d29
Patryk Nawolski <patryknawolski@gmail.com>
2019-02-21 15:34:56 +0100

    Add head via react-helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <title>React Contentful Pokedex</title>
+          <meta name="description" content="Pokedex built on top of React with Contenful CMS!"/>
+        </Helmet>
         <h1>Hello</h1>
       </div>
     );
