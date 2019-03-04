46223c16ded2ccb1bb5833aa93dad2f6b3dea6b4
Patryk Nawolski <patryknawolski@gmail.com>
2019-03-04 18:26:06 +0100

    Remove redux-axios-middleware

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,14 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import axios from 'axios';
-import axiosMiddleware from 'redux-axios-middleware';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 
-const client = axios.create({ //all axios can be used, shown in axios documentation
-  baseURL:'http://localhost:8080/api',
-  responseType: 'json'
-});
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(axiosMiddleware(client))));
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
