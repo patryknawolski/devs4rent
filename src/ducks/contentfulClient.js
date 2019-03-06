1bef2d16b9569a7ae731a4786a3851cf5a154856
Patryk Nawolski <patryknawolski@gmail.com>
2019-03-06 12:16:24 +0100

    Move contentfulCLient to it's own file

---
+++ src/ducks/contentfulClient.js
@@ -0,0 +1,6 @@
+import * as contentful from 'contentful';
+
+export default contentful.createClient({
+  space: 'y0qgj2b8sfqk',
+  accessToken: 'bf913d1c76e4583317ca443ad067efe78fa7855bd45172f4cd6c88416e70bc53'
+});
--- src/ducks/developers/actions.js
+++ src/ducks/developers/actions.js
@@ -1,10 +1,5 @@
-import * as contentful from 'contentful';
 import { GET_DEVELOPERS } from './actionTypes';
-
-const client = contentful.createClient({
-  space: 'y0qgj2b8sfqk',
-  accessToken: 'bf913d1c76e4583317ca443ad067efe78fa7855bd45172f4cd6c88416e70bc53'
-});
+import client from '../contentfulClient';
 
 export const getDevelopers = () => {
   return {
